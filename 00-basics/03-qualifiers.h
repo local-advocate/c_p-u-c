6ac3850bbb079a20602f5ab01762198626c88596
the-apprentice <theapprentice@ta.com>
2021-03-26 13:47:54 +0000

    renamed basics

---
+++ 00-basics/03-qualifiers.h
@@ -0,0 +1,9 @@
+#ifndef QUALIFIERS
+#define QUALIFIERS
+
+extern int extint1;
+extern void printextern();
+
+extern int inlfunc3();			// inline function defined in 03.c
+
+#endif /* QUALIFIERS */
---
+++ 00-basics/06-arg-parse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv){			// argc = num of arguments, argv = pointer to strings
+	
+	printf("Total arguments: %d\n", argc);
+	
+	if (argc < 2) {
+	       printf("No arguments. Exiting...\n"); 	// first arg = file name	
+	       exit(0);					// exit no error for now
+	}
+
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
+
+}
+
