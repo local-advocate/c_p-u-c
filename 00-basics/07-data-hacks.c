93f15223934cc0368432cdc9185ff052aa6ae3d3
the-apprentice <theapprentice@ta.com>
2021-03-26 18:35:14 +0000

    testing and making notes

--- 00-basics/07-data-hacks.c
+++ 00-basics/07-data-hacks.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>		// to convert one type to another
-#include <ctypes.h>		// to determine type
+#include <ctype.h>		// to determine type
 
 int main(){
+
+
+	/* STDLIB TYPE CONVERSION
+	 * 
+	 * FUNCTIONS
+	 * double atof(const char *str)		:	string to double (0.0 if can't be converted)
+	 *
+	 */
+
+
 
 	int i1 = 10;
 	int i2;
 
 	double d1 = 10.9d;
-	double d2;
+	double d2 = 0.0d;
 
 	char c1 = 'Z';
 	char c2;
@@ -17,11 +27,18 @@ int main(){
 	float f2;
 
 	char *str1  = "string1";
-	char str2[] = "string2";
+	char str2[] = "9&9.4a1";
 
 	long int l1 = 129900388833l;
 		
 
 
+
+	if ((d2 = atof(str2)) != 0) {
+		printf("String %s in double is %.5lf\n", str2, d2);
+	}
+	else {
+		printf("ERR OR ZERO. String: %s. Double: %.5lf\n", str2, d2);
+	}
 }
 
