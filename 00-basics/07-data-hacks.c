bba405c49d60cf56d10df818147486c57fe96bc4
the-apprentice <theapprentice@ta.com>
2021-03-26 18:13:01 +0000

    converting from one to another type

---
+++ 00-basics/07-data-hacks.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>		// to convert one type to another
+#include <ctypes.h>		// to determine type
+
+int main(){
+
+	int i1 = 10;
+	int i2;
+
+	double d1 = 10.9d;
+	double d2;
+
+	char c1 = 'Z';
+	char c2;
+
+	float f1 = 333.11f;
+	float f2;
+
+	char *str1  = "string1";
+	char str2[] = "string2";
+
+	long int l1 = 129900388833l;
+		
+
+
+}
+
