c1a3a17a7bba3d07a795de0e114e603713f2657d
the-apprentice <theapprentice@ta.com>
2021-03-26 14:05:06 +0000

    adding notes

--- 01-arg-parse/getopt.c
+++ 01-arg-parse/getopt.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-#include <getopt.h>
+#include <unistd.h>		// for getopt
 
+/* NOTES (ref: https://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html)
+ * command format: <command> -[option] <argument> 
+ * 		   if option takes argument: -option argument is same as -optionargument
+ * 		   if it does not	   : -optionoption is same as -option -option
+ * 		   -- terminates all options & arguments
 
 int main() {
 
