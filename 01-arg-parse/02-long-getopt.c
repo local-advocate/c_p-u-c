fc250fce6cdc032b76035df8a7fde5271f689bde
the-apprentice <theapprentice@ta.com>
2021-03-27 10:52:02 +0000

    adding notes for getopt_long

--- 01-arg-parse/02-long-getopt.c
+++ 01-arg-parse/02-long-getopt.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <getopt.h>	// for getopt long
+#include <stdlib.h>	// for exit
 
 /* NOTES (ref: https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Options.html)
+ *  
+ * GETOPT LONG
+ *
+ * struct option	: describes a single long option (getopt long takes array of this struct)
  * 
  *
  *
