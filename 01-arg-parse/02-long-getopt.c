08a72e77afc6538744916bc4644172bab64cdeee
the-apprentice <theapprentice@ta.com>
2021-03-27 10:42:18 +0000

    continuing from longopt

--- 01-arg-parse/02-long-getopt.c
+++ 01-arg-parse/02-long-getopt.c
@@ -1,1 +1,16 @@
-ref: https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Options.html
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* NOTES (ref: https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Options.html)
+ * 
+ *
+ *
+ */
+
+
+
+int main(int argc, char **argv){
+
+
+}
